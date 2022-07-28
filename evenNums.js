ff6e412a62dda36dff6d6ebc5353178a0f2919a4
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-07-28 10:43:26 -0400

    update code

--- evenNums.js
+++ evenNums.js
@@ -1,14 +1,14 @@
-// displayEvenNumbers
+// // displayEvenNumbers
 
-function displayEvenNumbers() {
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  let evenNumbers = [];
-  for (let i = 0; i <= numbers.length - 1; i++) {
-    if (numbers[i] % 2 === 0) {
-      evenNumbers.push(numbers[i]);
-    }
-  }
-  return evenNumbers;
-}
+// function displayEvenNumbers() {
+//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//   let evenNumbers = [];
+//   for (let i = 0; i <= numbers.length - 1; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       evenNumbers.push(numbers[i]);
+//     }
+//   }
+//   return evenNumbers;
+// }
 
-console.log(displayEvenNumbers());
+// console.log(displayEvenNumbers());
