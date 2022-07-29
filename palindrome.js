a96dcb0346816f44e32fcc97c66b7c4175d95d5c
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-07-29 09:36:13 -0400

    palindrome challenge, create a function that checks to see if a string is a palindrome or not

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+// Write a function to check whether a passed string is a palindrome or not. (Mom, Racecar etc.)
+
+// .split() than .reverse() and compare to orignal string..  then .join()
+
+// function palindrome(string) {
+//   const textChecker = string.split("");
+//   const reverseArray = textChecker.reverse();
+//   if (reverseArray.join("") === string) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(palindrome("nono"));
